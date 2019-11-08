d9f8d314b92ea23e7120c3415db9a7a78bd88f07
robertozdoba <rozzy1993@gmail.com>
2019-11-07 20:11:46 -0800

    map fragment and list view displays all markers and item click moves to chosen marker

--- app/src/main/java/com/bcit/parkfinder/Park.java
+++ app/src/main/java/com/bcit/parkfinder/Park.java
@@ -1,11 +1,14 @@
 package com.bcit.parkfinder;
 
-import java.util.ArrayList;
-
 public class Park {
 
     private String name;
     private double[] coordinates;
+
+    public Park(String name, double[] coordinates) {
+        this.name = name;
+        this.coordinates = coordinates;
+    }
 
     public String getName(){
         return this.name;
--- app/src/main/java/com/bcit/parkfinder/ParkListActivity.java
+++ app/src/main/java/com/bcit/parkfinder/ParkListActivity.java
@@ -11,6 +11,13 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -18,23 +25,33 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class ParkListActivity extends AppCompatActivity {
+public class ParkListActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private String TAG = ParkListActivity.class.getSimpleName();
     private ProgressDialog pDialog;
-    private ListView lv;
+    private ListView lvParks;
+
     // URL to get contacts JSON
     private static String SERVICE_URL = "https://opendata.vancouver.ca/api/records/1.0/search/?dataset=parks&rows=300&facet=specialfeatures&facet=facilities&facet=washrooms&facet=neighbourhoodname";
     private ArrayList<Park> parkList;
 
+    private GoogleMap mMap;
+    private String parkName;
+    private double[] parkCoordinates;
 
+    private ArrayList<double[]> coordinatesList;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_park_list);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
         parkList = new ArrayList<Park>();
-        lv = findViewById(R.id.parkList);
+        lvParks = findViewById(R.id.lvParkList);
         new GetContacts().execute();
     }
 
@@ -95,9 +112,7 @@ public class ParkListActivity extends AppCompatActivity {
                         String name = p.getString("name");
 
                         // Creating a Park object
-                        Park park = new Park();
-                        park.setName(name);
-                        park.setCoordinates(coordinates);
+                        Park park = new Park(name, coordinates);
 
                         parkList.add(park);
                     }
@@ -128,9 +143,7 @@ public class ParkListActivity extends AppCompatActivity {
                                 .show();
                     }
                 });
-
             }
-
             return null;
         }
 
@@ -145,22 +158,68 @@ public class ParkListActivity extends AppCompatActivity {
             ParksAdapter adapter = new ParksAdapter(ParkListActivity.this, parkList);
 
             // Attach the adapter to a ListView
-            lv.setAdapter(adapter);
+            lvParks.setAdapter(adapter);
 
-            // On Click
-            lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            // adds all park markers to the map fragment
+            addAllParkMarkers();
+
+            // move camera position to location of chosen park
+            lvParks.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 @Override
-                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
-                {
-                    Intent intent = new Intent(ParkListActivity.this, ParkDetailActivity.class);
-                    Park park = parkList.get(i);
-                    intent.putExtra("name", park.getName());
-                    intent.putExtra("coordinates", park.getCoordinates());
-                    startActivity(intent);
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    // get park details
+                    Park park = parkList.get(position);
+                    parkName = park.getName();
+                    parkCoordinates = park.getCoordinates();
+
+                    // add marker and move camera position to park location
+                    LatLng parkLocation = new LatLng(parkCoordinates[0], parkCoordinates[1]);
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(parkLocation, 12.5f));
                 }
             });
 
         }
     }
+
+    public void addAllParkMarkers() {
+        mMap.clear();
+        for(Park park: parkList) {
+
+            // Get park details
+            String parkName = park.getName();
+            double[] coordinates = park.getCoordinates();
+
+            // Add a marker for park in parkList
+            LatLng parkLocation = new LatLng(coordinates[0], coordinates[1]);
+            mMap.addMarker(new MarkerOptions().position(parkLocation).title(parkName));
+        }
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.clear();
+        // moves camera position of the map over vancouver
+        LatLng vancouver = new LatLng(49.246292, -123.116226);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(vancouver, 11.5f));
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    // On Click
+//            lvParks.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//                @Override
+//                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+//                {
+//                    Intent intent = new Intent(ParkListActivity.this, ParkDetailActivity.class);
+//                    Park park = parkList.get(i);
+//                    intent.putExtra("name", park.getName());
+//                    intent.putExtra("coordinates", park.getCoordinates());
+//                    startActivity(intent);
+//                }
+//            });
 
 }
